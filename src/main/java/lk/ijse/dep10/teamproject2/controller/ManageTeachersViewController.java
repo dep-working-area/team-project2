1925ad33f9c490a379cd2a5e02a7367b1242dd8e
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2023-03-23 18:40:35 +0530

    Finalize UI implementation

---
+++ src/main/java/lk/ijse/dep10/teamproject2/controller/ManageTeachersViewController.java
@@ -0,0 +1,48 @@
+package lk.ijse.dep10.teamproject2.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageTeachersViewController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewTeacher;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblTeachers;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewTeacherOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
+
